99cdd0bdb318a0897df32f35627482f3ff20e451
Eugene Volodin <wubanga.main@gmail.com>
2022-01-14 12:01:13 +0300

    feat: Search Component Appearance

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,10 +1,12 @@
-import { useState } from 'react';
-
+import { SearchIcon } from '../Icons/SearchIcon';
 import './Search.css';
 
 export const Search = () => {
   return (
     <form action="" className="search">
+      <i className="search__icon">
+        <SearchIcon />
+      </i>
       <input className="search__input" type="search" placeholder="Search..." />
     </form>
   );
