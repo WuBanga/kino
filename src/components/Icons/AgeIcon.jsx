ac3a358b8d447e4f6aefa7d03227c9c618dda771
Eugene Volodin <wubanga.main@gmail.com>
2022-01-24 18:04:06 +0300

    feat: AgeIcon

---
+++ src/components/Icons/AgeIcon.jsx
@@ -0,0 +1,9 @@
+import './AgeIcon.css';
+export const AgeIcon = () => {
+  return (
+    <div className="age">
+      <p className="age__text">18</p>
+      <p className="age__plus">+</p>
+    </div>
+  );
+};
